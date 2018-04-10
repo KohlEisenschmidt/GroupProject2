f3f972284c66a038e46ea66e6ab12d9f550a0652
Melissa Saldana <melissasaldana@wifi_stu-10-38-95-192.uncc.edu>
2018-04-10 10:44:14 -0400

    initial html

--- server.js
+++ server.js
@@ -1,38 +1,57 @@
-// *****************************************************************************
-// Server.js - This file is the initial starting point for the Node/Express server.
-//
-// ******************************************************************************
-// *** Dependencies
-// =============================================================
-var express = require("express");
-var bodyParser = require("body-parser");
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
+// Initialize Firebase (ADD YOUR OWN DATA)
+var config = {
+    apiKey: "AIzaSyATOhdQTrlYEQo5ZRlQRvl12wCq2EumQ8s",
+    authDomain: "contactform-c5fa8.firebaseapp.com",
+    databaseURL: "https://contactform-c5fa8.firebaseio.com",
+    projectId: "contactform-c5fa8",
+    storageBucket: "contactform-c5fa8.appspot.com",
+    messagingSenderId: "561182159021"
+  };
+  firebase.initializeApp(config);
+  
+  // Reference messages collection
+  var messagesRef = firebase.database().ref('messages');
+  
+  // Listen for form submit
+  document.getElementById('contactForm').addEventListener('submit', submitForm);
+  
+  // Submit form
+  function submitForm(e){
+    e.preventDefault();
+  
+    // Get values
+    var name = getInputVal('name');
+    var email = getInputVal('email');
+    var subject = getInputVal('subject');
+    var message = getInputVal('message');
+  
+    // Save message
+    saveMessage(name, email, subject, message);
 
-// Requiring our models for syncing
-var db = require("./models");
-
-// Sets up the Express app to handle data parsing
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-// parse application/json
-app.use(bodyParser.json());
-
-// Static directory
-app.use(express.static("public"));
-
-// Routes
-// =============================================================
-require("./routes/api-routes.js")(app);
-
-// Syncing our sequelize models and then starting our Express app
-// =============================================================
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+  
+    // Clear form
+    document.getElementById('contactForm').reset();
+  }
+  // Show alert
+  document.querySelector('.alert').style.display = 'block';
+  
+  // Hide alert after 3 seconds
+  setTimeout(function(){
+    document.querySelector('.alert').style.display = 'none';
   });
-});
+  // Function to get form values
+  function getInputVal(id){
+    return document.getElementById(id).value;
+  }
+  
+  // Save message to firebase
+  function saveMessage(name, email, subject, message){
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+      name: name,
+      email:email,
+      subject:subject,
+      message: message
+    });
+  }
