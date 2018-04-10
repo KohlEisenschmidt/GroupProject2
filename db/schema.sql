5a28566c031e51f1922fda94aa4fa811fe56bc67
Kohl Eisenschmidt <kohleisenschmidt@gmail.com>
2018-04-10 13:28:16 -0400

    committing kohls db to master

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,22 @@
--- Drops the todolist if it exists currently --
-DROP DATABASE IF EXISTS todolist;
--- Creates the "todolist" database --
-CREATE DATABASE todolist;
+DROP DATABASE IF EXISTS WBR_db ;
+
+-- ### Schema
+
+CREATE DATABASE WBR_db;
+USE WBR_db;
+
+CREATE TABLE profiles
+(
+	id int NOT NULL AUTO_INCREMENT,
+	person_name VARCHAR(50) NOT NULL,
+    gender VARCHAR(50) NOT NULL,
+	activity VARCHAR(50) NOT NULL,
+    area VARCHAR(50) NOT NULL,
+    intensity VARCHAR(50) NOT NULL,
+    time_in_min INTEGER(50) NOT NULL,
+    total INTEGER(50) NOT NULL,
+    
+    image VARCHAR(2083) NOT NULL,
+    
+	PRIMARY KEY (id)
+);
